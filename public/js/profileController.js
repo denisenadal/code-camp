6289dc9adb8acc557ed6bfd55bdae407b60af316
Denise Nadal <denisenadal@outlook.com>
2017-11-11 05:58:15 -0700

    mostly ready

--- public/js/profileController.js
+++ public/js/profileController.js
@@ -10,4 +10,26 @@ angular.module('resumaker').controller('profileController',function($scope,$loca
 		//else send POST
 	};
 
+	angular.element('.add-repeater').click(function(){
+		fieldRepeater(this);
+	});
+
+	function fieldRepeater(clicked){
+		var type = angular.element(clicked).attr('id').slice(4);
+		console.log(type);
+		var lastEntry = angular.element('.'+type+'-set').last();
+		console.log(lastEntry);
+		var num = lastEntry.attr('id').replace(/^\D+/g,'');
+		console.log(lastEntry.attr('id'));
+		num = parseInt(num);
+		console.log("old numis: "+num);
+		var newID = type+(num+1);
+		console.log(newID);
+		var newEntry = angular.element('#'+type+'0').clone(true,true).attr('id',newID);
+		newEntry.html(function(){
+			return angular.element(this).html().replace(new RegExp(type+0, "g"), type+(num+1) );
+		});
+		newEntry.insertAfter(lastEntry).show();
+	}
+
 });
--- public/js/resumeController.js
+++ public/js/resumeController.js
@@ -15,6 +15,22 @@ angular.module('resumaker').controller('resumeController',function($scope,$locat
 			$scope.userDetails = false;
 		});
 	};
-	 $scope.getDetails();
+	if($scope.currentUser){
+		$scope.getDetails();
+	}
+
+	angular.element('#builder .button').click(function(){
+		angular.element('#builder .button').removeClass('is-outlined');
+		angular.element(this).addClass('is-outlined');
+	});
+
+	angular.element('#builder .res-template').click(function(){
+		angular.element('#builder').removeClass('invalid');
+		var color = angular.element('#builder .button.is-outlined').attr('value');
+		console.log("color:"+color);
+		angular.element('#print-res .stripe').addClass('is-'+color);
+		angular.element('#print-res h2, #print-res h1').addClass('has-text-'+color);
+	});
+
 
 });
