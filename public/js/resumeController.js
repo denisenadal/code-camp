ad1116bef234b2a723e58f5225abad51e7eca294
Denise Nadal <denisenadal@outlook.com>
2017-11-11 07:21:00 -0700

    added new template

--- public/js/resumeController.js
+++ public/js/resumeController.js
@@ -5,7 +5,7 @@ angular.module('resumaker').controller('resumeController',function($scope,$locat
 	console.log($scope.currentUser);
 
 	$scope.getDetails = function(){
-		console.log("getDetails is tring");
+		console.log("getDetails is trying");
 		restservice.getEverything($scope.currentUser.uid, function(response){
 			console.log(response);
 			$scope.userDetails = response;
@@ -25,11 +25,15 @@ angular.module('resumaker').controller('resumeController',function($scope,$locat
 	});
 
 	angular.element('#builder .res-template').click(function(){
+		var resType = angular.element(this).attr("data-temp");
+		angular.element('#print-res').prepend('<link rel="stylesheet" href="css/resume-'+resType+'.css">');
+
 		angular.element('#builder').removeClass('invalid');
 		var color = angular.element('#builder .button.is-outlined').attr('value');
 		console.log("color:"+color);
-		angular.element('#print-res .stripe').addClass('is-'+color);
+		angular.element('#print-res .stripe, #print-res .initials' ).addClass('is-'+color);
 		angular.element('#print-res h2, #print-res h1').addClass('has-text-'+color);
+
 	});
 
 
